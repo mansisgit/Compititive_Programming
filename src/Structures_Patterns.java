cf9a6a9ea205591124251bc14c21331a4a35b850
mansisgit <mancc81@gmail.com>
2025-10-12 22:17:28 +0530

    hil

--- src/Structures_Patterns.java
+++ src/Structures_Patterns.java
@@ -1,5 +1,11 @@
 public class Structures_Patterns {
     public static void main(String[] args) {
-
+        /*
+        linear data structures :
+        Array
+        LinkedList
+        Stack-->either made by LL or Array
+        Queue-->either made by LL or Array
+        **/
     }
 }
