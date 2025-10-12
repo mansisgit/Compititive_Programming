710a81f831ad2733f8f90cc13d40e79f3fa09727
mansisgit <mancc81@gmail.com>
2025-10-12 21:38:30 +0530

    hil

--- src/Halloumni_Boxes.java
+++ src/Halloumni_Boxes.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 
 public class Halloumni_Boxes {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+
 
     }
 }
