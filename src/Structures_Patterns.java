f4b2f4618e6a2e8d3ca49938a2585a0f49fbccde
mansisgit <mancc81@gmail.com>
2025-10-12 22:26:41 +0530

    hil

--- src/Structures_Patterns.java
+++ src/Structures_Patterns.java
@@ -1,19 +1,27 @@
 public class Structures_Patterns {
+    /*
+       linear data structures :
+       Array
+       LinkedList
+       Stack-->either made by LL or Array--->pile of books
+       Queue-->either made by LL or Array--->queue at store
+
+       Non-linear data structures:(Dynamic data Structure)
+       graph
+       tree(subset of graph)
+
+       Hash Based:
+       HashMap
+       Hashset
+       **/
+
+    /*
+    ARRAY
+    fixed data structure
+    index position starting form 0
+    **/
     public static void main(String[] args) {
-        /*
-        linear data structures :
-        Array
-        LinkedList
-        Stack-->either made by LL or Array--->pile of books
-        Queue-->either made by LL or Array--->queue at store
 
-        Non linear data structures:(Dynamic data Structure)
-        graph
-        tree(subset of graph)
 
-        Hash Based:
-        HashMap
-        Hashset
-        **/
     }
 }
