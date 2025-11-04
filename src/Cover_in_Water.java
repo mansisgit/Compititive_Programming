8fdbe317445c334d37b285ed1736920dd2d34a40
mansisgit <mancc81@gmail.com>
2025-11-04 22:02:31 +0530

    hil

---
+++ src/Cover_in_Water.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Cover_in_Water {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+
+        while(tests-->0){
+            int cells = sc.nextInt();
+            String s = sc.next();
+
+        }
+    }
+}
--- src/Halloumni_Boxes.java
+++ src/Halloumni_Boxes.java
@@ -4,7 +4,17 @@ public class Halloumni_Boxes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int tests = sc.nextInt();
+        while(tests-->0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
 
+            int[] arr = new int[n];
+            for(int i =0;i<n;i++){
+                arr[i] = sc.nextInt();
+            }
+
+            
+        }
 
     }
 }
