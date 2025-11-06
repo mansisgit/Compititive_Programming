5ac9d5d838780418bbde921826952c9fe63eac9c
mansisgit <mancc81@gmail.com>
2025-11-06 17:28:49 +0530

    carxy

--- src/Cover_in_Water.java
+++ src/Cover_in_Water.java
@@ -8,7 +8,17 @@ public class Cover_in_Water {
         while(tests-->0){
             int cells = sc.nextInt();
             String s = sc.next();
+            int count=0;
+            if(s.charAt(0) == '.') count=1;
+            else count =0;
 
+            for(int i=2;i<cells;i++){
+                if(s.charAt(i-1) == s.charAt(i) && s.charAt(i) =='.'){
+                    count++;
+                    i++;
+                }
+            }
+            System.out.println(count);
         }
     }
 }
