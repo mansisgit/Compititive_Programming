b06229c4144fe9acbe9b92464c85388eef47dbff
mansisgit <mancc81@gmail.com>
2025-11-12 23:48:34 +0530

    carxysdf

--- src/DontTryToCount.java
+++ src/DontTryToCount.java
@@ -13,6 +13,13 @@ public class DontTryToCount {
             String updatable = sc.next();
             String tobechecked = sc.next();
 
+            String x1 = updatable + updatable;//1 opration
+            String x2 = x1 + x1;//2 operations
+            String x3 = x2 + x2;//3 operations
+            String x4 = x3 + x3;//4 operations
+            String x5 = x4 + x4;//5 operations
+
+            int operations = -1;
 
 
         }
