738f07c44eddfc7091e9873c5313525842be84f1
mansisgit <mancc81@gmail.com>
2025-11-12 23:29:57 +0530

    carxysdf

---
+++ src/DontTryToCount.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class DontTryToCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+
+        while (tests-->0){
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            String updatable = sc.next();
+            String tobechecked = sc.next();
+
+
+
+        }
+    }
+}
