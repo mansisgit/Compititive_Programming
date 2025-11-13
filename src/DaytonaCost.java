272940b3840241480720bfb4f1b8c410e95955d5
mansisgit <mancc81@gmail.com>
2025-11-13 22:55:59 +0530

    carxysdf

--- src/DaytonaCost.java
+++ src/DaytonaCost.java
@@ -6,14 +6,7 @@ public class DaytonaCost {
         int tests = sc.nextInt();
 
         while (tests-->0){
-            int n = sc.nextInt();
-            int x = sc.nextInt();
-            int y = sc.nextInt();
 
-            int cost_option1 = n * x;
-            int cost_option2 = y;
-
-            System.out.println(Math.min(cost_option1, cost_option2));
         }
     }
 }
