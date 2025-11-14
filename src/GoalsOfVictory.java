f82b3087d8dc826b2818fbac82cc366c360e486f
mansisgit <mancc81@gmail.com>
2025-11-14 22:42:55 +0530

    carxysdf

--- src/GoalsOfVictory.java
+++ src/GoalsOfVictory.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class GoalsOfVictory {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+        while(tests-->0){
+
+        }
+    }
 }
