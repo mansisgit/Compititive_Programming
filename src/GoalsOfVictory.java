9990040006f71262111c247f585b36d0cf868928
mansisgit <mancc81@gmail.com>
2025-11-14 23:11:24 +0530

    carxysdf

--- src/GoalsOfVictory.java
+++ src/GoalsOfVictory.java
@@ -5,6 +5,12 @@ public class GoalsOfVictory {
         Scanner sc = new Scanner(System.in);
         int tests = sc.nextInt();
         while(tests-->0){
+            int no_of_teams = sc.nextInt();
+            int[] arr = new int[no_of_teams-1];
+            for(int i=0;i<no_of_teams-1;i++){
+                arr[i] = sc.nextInt();
+            }//input taken
+
 
         }
     }
