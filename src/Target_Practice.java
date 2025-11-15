961a4d49f08a6da003952712689495db44bba9b5
mansisgit <mancc81@gmail.com>
2025-11-15 23:21:52 +0530

    carxysdf

---
+++ src/Target_Practice.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Target_Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+        while(tests-->0){
+            char[][] arr = new char[10][10];
+
+            for(int i=0;i<10;i++){
+                {1,1,1,1,1,1,1,1,1,1,1};
+                {1,2,2,2,2,2,2,2,2,2,1};
+                {1,2,3,3,3,3,3,3,3,2,1};
+                {1,2,3,4,4,4,4,4,3,2,1};
+                {1,2,3,4,1,1,1,1,3,2,1};
+                {1,2,3,4,1,1,1,1,3,2,1};
+                {1,2,3,4,1,1,1,1,3,2,1};
+                {1,2,3,3,3,3,3,3,3,2,1};
+                {1,2,2,2,2,2,2,2,2,2,1};
+                {1,1,1,1,1,1,1,1,1,1,1};
+            }
+            int score =0;
+            for(int i=0;i<10;i++){
+                for(int j=0;j<10;j++){
+                    if(arr[i][j]=='X'){
+                        
+                    }
+                }
+            }
+
+        }
+    }
+}
