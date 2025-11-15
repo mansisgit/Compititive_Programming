e13e841aa1b87ba309ae83ee95b53f86f17ed503
mansisgit <mancc81@gmail.com>
2025-11-15 14:19:58 +0530

    carxysdf

--- src/GoalsOfVictory.java
+++ src/GoalsOfVictory.java
@@ -7,9 +7,15 @@ public class GoalsOfVictory {
         while(tests-->0){
             int no_of_teams = sc.nextInt();
             int[] arr = new int[no_of_teams-1];
+            int forgotten_efficiency = 0;
             for(int i=0;i<no_of_teams-1;i++){
                 arr[i] = sc.nextInt();
-            }//input taken
+                forgotten_efficiency+=arr[i];
+            }
+            //input taken
+            forgotten_efficiency = forgotten_efficiency*(-1);
+            System.out.println(forgotten_efficiency);
+
 
 
         }
