edfd53b670d77dc964e2f3e5d1c7cecaccc3576e
mansisgit <mancc81@gmail.com>
2025-11-16 13:50:38 +0530

    carxysdf

--- src/AmbitiousKid.java
+++ src/AmbitiousKid.java
@@ -9,14 +9,15 @@ public class AmbitiousKid {
 
         for(int i=0;i<length;i++){
             arr[i] = sc.nextInt();
+            arr[i]= Math.abs(arr[i]);
         }
         //input taken
 
        int min = arr[0];
         for(int ele : arr){
-            if(ele<0) ele*=(-1);
             min = Math.min(ele,min);
         }
 
+        System.out.println(min);
     }
 }
