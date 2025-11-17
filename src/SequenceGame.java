698e7360a5a37fa0b20e52ae3294c75a158aa6f6
mansisgit <mancc81@gmail.com>
2025-11-17 15:04:54 +0530

    carxysdf

---
+++ src/SequenceGame.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SequenceGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tests = sc.nextInt();
+        while(tests-->0){
+            int length = sc.nextInt();
+            int [] a = new int[length];
+            for(int i=0;i<length;i++){
+                a[i] = sc.nextInt();
+            }
+            //input taken
+
+
+        }
+    }
+}
