5ab692762ec48456a755cbaa5e253e03a47dd32f
mansisgit <mancc81@gmail.com>
2025-11-17 15:07:47 +0530

    carxysdf

--- src/UnitedWeStand.java
+++ src/UnitedWeStand.java
@@ -4,5 +4,8 @@ public class UnitedWeStand {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int tests = sc.nextInt();
+        while(tests-->0){
+
+        }
     }
 }
