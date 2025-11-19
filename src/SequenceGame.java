00aa3ebc5932a5f7795dec5cb5bde02a6ff5a67c
mansisgit <mancc81@gmail.com>
2025-11-19 19:30:33 +0530

    carxysdf

--- src/SequenceGame.java
+++ src/SequenceGame.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SequenceGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int tests = sc.nextInt();
-        while(tests-->0){
+        while (tests-- > 0) {
             int length = sc.nextInt();
-            int [] b = new int[length];
-            for(int i=0;i<length;i++){
+            int[] b = new int[length];
+            for (int i = 0; i < length; i++) {
                 b[i] = sc.nextInt();
             }
 
@@ -16,18 +17,16 @@ public class SequenceGame {
             ArrayList<Integer> a = new ArrayList<>();
             a.add(b[0]);//first number is added
 
-            for(int i=1;i<length;i++){
-                if(b[i-1]<b[i]){
+            for (int i = 1; i < length; i++) {
+                if (b[i - 1] < b[i]) {
                     a.add(b[i]);
                 } else {
                     a.add(b[i]);
-                    a.add(b[i]);
+
                 }
+
             }
-
-            System.out.println(a.toArray());
-
-
+            System.out.println(Arrays.toString(a.toArray()));
 
         }
     }
