82359d80a61d8c516a836f012164583f909be42e
mansisgit <mancc81@gmail.com>
2026-01-08 12:35:10 +0530

    carxysdf

--- src/Program.java
+++ src/Program.java
@@ -35,13 +35,11 @@ public class Program {
         }
         System.out.println("MSB is at position: "+power1);
 
-        //------Bit Manipulation
+        //------Bit Manipulation-----
         //write a code to toggle the nth bit of a number
         int c = 5; //101
         int nthbit = 1;
-
-
-
+        System.out.println( c ^ (1<<nthbit));
 
     }
 }
