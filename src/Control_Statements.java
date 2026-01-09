e5f4acb6dd9d7b6534f07d819d58794e97bddcfa
mansisgit <mancc81@gmail.com>
2026-01-09 11:57:04 +0530

    carxysdf

--- src/Control_Statements.java
+++ src/Control_Statements.java
@@ -56,16 +56,21 @@ public class Control_Statements {
         }
         System.out.println("------------------------------");
         //nested for loop to print a pattern
-        int elet =2;
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                System.out.print(elet+" ");
-                elet++;
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(i+j);
             }
             System.out.println();
-            elet-=4;
-        }
-        //improved logic for above
 
+        }
+        System.out.println("------------------------------");
+        for(int i=5;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------");
     }
 }
