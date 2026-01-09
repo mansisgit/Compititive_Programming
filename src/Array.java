c030e0d87d6bf5f779ed14d901ad8fb6efef4a94
mansisgit <mancc81@gmail.com>
2026-01-09 15:37:52 +0530

    carxysdf

--- src/Array.java
+++ src/Array.java
@@ -2,6 +2,14 @@ public class Array {
     public static void main(String[] args) {
         //write a code to find a factor of each element in an array
         int[] arr = {10,15,21,33,40};
-
+        for(int ele: arr){
+            System.out.println("Factors of "+ele+" are: ");
+            for(int i=1;i<ele;i++){
+                if(ele%i==0){
+                    System.out.print(i+" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
