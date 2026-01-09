bafd0ff7273795405d7347886d59d2e157168b6a
mansisgit <mancc81@gmail.com>
2026-01-09 10:45:07 +0530

    carxysdf

--- src/Control_Statements.java
+++ src/Control_Statements.java
@@ -1,5 +1,14 @@
 public class Control_Statements {
     public static void main(String[] args) {
-        
+        boolean fine = true;
+        if(true) System.out.println("hi");//if() must be a boolean data type
+        //write a code to check the divisibility if number is divisible by both the number return both
+        //if number is divisible by one of any number but not by second then return that number
+        int a =30,b=5,c=10;
+        if(a%b==0 && a%c==0) System.out.println("both");
+        else if(a%b==0) System.out.println(b);
+        else if(a%c==0) System.out.println(c);
+        else System.out.println("None");
+
     }
 }
