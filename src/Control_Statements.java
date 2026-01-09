13b524894139000073ffa6796d86931718ff3d7f
mansisgit <mancc81@gmail.com>
2026-01-09 11:23:43 +0530

    carxysdf

--- src/Control_Statements.java
+++ src/Control_Statements.java
@@ -2,6 +2,7 @@ public class Control_Statements {
     public static void main(String[] args) {
         boolean fine = true;
         if(true) System.out.println("hi");//if() must be a boolean data type
+
         //write a code to check the divisibility if number is divisible by both the number return both
         //if number is divisible by one of any number but not by second then return that number
         int a =30,b=5,c=10;
@@ -9,6 +10,28 @@ public class Control_Statements {
         else if(a%b==0) System.out.println(b);
         else if(a%c==0) System.out.println(c);
         else System.out.println("None");
+
+        //write a code to find the arithmatic progression series where first number, nth number and difference will be given
+        int first = 2;
+        int nth = 50;
+        int diff = 2;
+        int count =0;
+        int ele =0;
+        while(ele<=nth){
+            ele = first + (count*diff);
+            System.out.print(ele + " ,");
+            count++;
+        }
+
+        //write a code to find the digits of number
+        int num=12345;
+        int element = num;
+        int digit =0;
+        while(element>0){
+            element=element/10;
+            digit++;
+        }
+        System.out.println(digit);
 
     }
 }
