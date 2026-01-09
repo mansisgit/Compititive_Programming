df9e25af0d05ab7d052653e821c9bed8c3822ba5
mansisgit <mancc81@gmail.com>
2026-01-09 15:35:38 +0530

    carxysdf

---
+++ src/Array.java
@@ -0,0 +1,7 @@
+public class Array {
+    public static void main(String[] args) {
+        //write a code to find a factor of each element in an array
+        int[] arr = {10,15,21,33,40};
+
+    }
+}
