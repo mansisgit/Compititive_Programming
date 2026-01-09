bfa87f196490446bba5d4bcb28d0b53932c87663
mansisgit <mancc81@gmail.com>
2026-01-09 12:21:19 +0530

    carxysdf

--- src/Control_Statements.java
+++ src/Control_Statements.java
@@ -59,7 +59,7 @@ public class Control_Statements {
 
         for(int i=1;i<=5;i++){
             for(int j=1;j<=5;j++){
-                System.out.print(i+j);
+                System.out.print(i+j+" ");
             }
             System.out.println();
 
@@ -72,5 +72,38 @@ public class Control_Statements {
             System.out.println();
         }
         System.out.println("------------------------------");
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------");
+
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------");
+        for(int i=1;i<=5;i++){
+            //for spaces
+            for(int k=4;k>=i;k--){
+                System.out.print(" ");
+            }
+            //for numbers
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------");
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
