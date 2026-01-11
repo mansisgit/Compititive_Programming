aa4ac4bfede965499a97d15b7587c7e955f3071b
mansisgit <mancc81@gmail.com>
2026-01-11 23:42:15 +0530

    carxysdf

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -2,6 +2,6 @@ public class RemoveDuplicates {
     public static void main(String[] args) {
 //first sorting the array and then returning the length of the sorted part only
 int [] arr = {1,2,2,3,3};
-
+//12th and 10th wale bharna hai
     }
 }
