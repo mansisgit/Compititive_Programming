89b8f3cc50f0b44593be71f6960ce5c86154ad0b
mansisgit <mancc81@gmail.com>
2026-01-11 23:44:23 +0530

    carxysdf

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -5,5 +5,7 @@ int [] arr = {1,2,2,3,3};
 //12th and 10th wale bharna hai
         //uske bad babbi ka bhi bharna hai
 //and then saare ke saare exams ka syllabus check yaphir dates dekh lo
+        // engineering wale and entc wale
+        //rrb ssc nabard gate rbi ese
     }
 }
