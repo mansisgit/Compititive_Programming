56c33ddaca251024a498c417f28ca2e92c8dea1f
mansisgit <mancc81@gmail.com>
2026-01-13 16:30:03 +0530

    carxysdf

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -2,7 +2,14 @@ import java.util.Arrays;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        int[] arr = {0,0,1,1,1,2,3};
+        int[] arr = {0,0,1,1,1,3,3};
+        int n= arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j]) i++;
+                else
+            }
+        }
         System.out.println(Arrays.toString(arr));
 
         }
