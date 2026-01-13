025b1984c8b3dbae689ff1e827b4a24c3ef4962a
mansisgit <mancc81@gmail.com>
2026-01-13 16:26:54 +0530

    carxysdf

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -3,19 +3,6 @@ import java.util.Arrays;
 public class RemoveDuplicates {
     public static void main(String[] args) {
         int[] arr = {0,0,1,1,1,2,3};
-
-//first sorting the array and then returning the length of the sorted part only
-        int compare_index = 0;
-        int fast = 1;
-        while(compare_index <arr.length-1 && fast <arr.length){
-            if(arr[compare_index] == arr[fast]) fast++;
-            else {
-                arr[compare_index+1] = arr[fast];
-                compare_index ++;
-            }
-
-        }
-
         System.out.println(Arrays.toString(arr));
 
         }
