5e9c4c48b7b9d85b381d2cef58661669fbf0082d
mansisgit <mancc81@gmail.com>
2026-01-13 11:08:38 +0530

    carxysdf

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -4,8 +4,9 @@ public class RemoveDuplicates {
 
 //first sorting the array and then returning the length of the sorted part only
         int count =0;
-        for(int i=0;i<arr.length;i++){
+
+
 
         }
     }
-}
+
