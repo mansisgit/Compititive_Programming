49d54bd103f7f122f8f9819c8d9118e46104ed89
mansisgit <mancc81@gmail.com>
2026-01-14 11:45:27 +0530

    carxysdf

--- src/Collections.java
+++ src/Collections.java
@@ -1,2 +1,7 @@
+import java.util.ArrayList;
 public class Collections {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+    }
 }
