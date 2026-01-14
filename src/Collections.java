8c5e7f09b2d9fd1b3e5248fed8b42453c7d637bd
mansisgit <mancc81@gmail.com>
2026-01-14 12:12:26 +0530

    carxysdf

--- src/Collections.java
+++ src/Collections.java
@@ -3,5 +3,16 @@ public class Collections {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(10);
+        list.add(16);
+
+        list.add(45);
+        list.add(18);
+        list.add(85);
+        list.remove(1);
+
+        list.get(0);
+        System.out.println(list.indexOf(18));
+        System.out.println(list.remove(1));
+
     }
 }
---
+++ src/GroceryShopping.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GroceryShopping {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> items = new ArrayList<>(5);
+        for (int i = 0; i < 5; i++) {
+            items.add(sc.next());
+        }
+        System.out.println(items.get(0) + " " + items.get(2));
+        items.set(1, sc.next());
+        items.remove(3);
+        items.add(sc.next());
+        System.out.println("the final list is:");
+
+        String res = Arrays.toString(items.toArray());
+        System.out.println(res);
+    }
+}
