f6c59291722720dcde1983224bbd1cfc8f61c990
mansisgit <mancc81@gmail.com>
2026-01-17 14:19:37 +0530

    carxysdf

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class BankAccount {
     int bankBalance;
     int accountNo;
@@ -13,6 +15,26 @@ public class BankAccount {
 
     public void checkBalance(){
         System.out.println("Total Balance: "+bankBalance);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name:");
+        String name = sc.nextLine();
+        System.out.println("Enter account number: ");
+        int accountNo = sc.nextInt();
+
+        //oject creation
+        BankAccount obj = new BankAccount();
+
+        System.out.println("Enter the operation:");
+        System.out.print("1:deposite"+"----"+"2.withdraw"+"-----"+"3.balance");
+        int choice = sc.nextInt();
+        switch (choice){
+            case 1 -> obj.deposit(sc.nextInt());
+            case 2 -> obj.withdraw(sc.nextInt());
+            case 3 -> obj.checkBalance();
+        }
     }
 
 }
