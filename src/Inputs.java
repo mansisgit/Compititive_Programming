27e9daa8e9df05dd6fd10489bd8180398e4fb7a6
mansisgit <mancc81@gmail.com>
2026-01-19 11:49:15 +0530

    carxysdf

--- src/Inputs.java
+++ src/Inputs.java
@@ -1,5 +1,7 @@
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.util.*;
+
 public class Inputs {
     public static void main(String[] arr) throws Exception {
         InputStreamReader isr = new InputStreamReader(System.in);
@@ -10,5 +12,12 @@ public class Inputs {
         int n2 = Integer.parseInt(arra[1]);
         int sum = n2+n1;
         System.out.println(sum);
+
+        HashSet<Integer> set = new HashSet<>();
+
+        boolean added = set.add(5);
+        System.out.println(added);
+        boolean added1 = set.add(5);
+        System.out.println(added1);
     }
 }
